ec230c336e46608089cefd46ae4135b9ab3ada1c
Abhinav <30766543+SaintOfAges@users.noreply.github.com>
2018-04-18 20:48:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,11 +5,21 @@ from   discord import errors
 
 bot = commands.Bot(command_prefix="ik", description="Discripto")
 @bot.command()
+###
+import functools, youtube_dl
+url = 'youtube url'
+opts = {"format": 'webm[abr>0]/bestaudio/best',"ignoreerrors": True,"default_search": "auto","source_address": "0.0.0.0",'quiet': True}
+ydl = youtube_dl.YoutubeDL(opts)
+func = functools.partial(ydl.extract_info, url, download=False)
+info = func()
+player = discord.FFmpegPCMAudio(info['url'])
+###
 async def yeet(ctx):
     """Sends a simple Hello Message"""
     await ctx.send("Sup!!")
 @bot.command()
 async def mime(ctx, *, something):
+	"""Repeats What you Say!"""
     await ctx.send(something)
 #bot.remove_command('help')
 #@bot.command()
@@ -17,12 +27,15 @@ async def mime(ctx, *, something):
 	#await ctx.send("Figure it out Yourself!")
 @bot.command()
 async def cult(ctx, member: discord.User):
+	"""Didnt fix that one"""
     await member.send("Wanna Join the Illuminati?")
 @bot.command()
 async def shout(ctx):
+	"""Mentions"""
 	await ctx.send(ctx.author.mention)
 @bot.command()
 async def mention(ctx):
+	"""Mention"""
     await ctx.author.send(ctx.author.mention)
 @bot.event
 async def on_member_join(member):
@@ -30,6 +43,7 @@ async def on_member_join(member):
     await member.send("Welcome to {}!".format(guild.name))
 @bot.command()
 async def add(ctx,a,b):
+	"""Simple Addition"""
 	c=int(a)+int(b)
 	await ctx.send(c)
 
