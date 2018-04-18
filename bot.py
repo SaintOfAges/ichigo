eb5d1d4881ce5eb8db6b0bbd4dcb1d7e2f5e81ea
Abhinav <30766543+SaintOfAges@users.noreply.github.com>
2018-04-18 21:01:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,22 @@
 import discord
 from discord.ext import commands
+import sys
 import discord
+import time
+import sys
+import os
+import random
+import subprocess
+import traceback
 from   discord import errors
 
 bot = commands.Bot(command_prefix="ik", description="Discripto")
-
-"""
-import functools, youtube_dl
-url = 'youtube url'
-opts = {"format": 'webm[abr>0]/bestaudio/best',"ignoreerrors": True,"default_search": "auto","source_address": "0.0.0.0",'quiet': True}
-ydl = youtube_dl.YoutubeDL(opts)
-func = functools.partial(ydl.extract_info, url, download=False)
-info = func()
-player = discord.FFmpegPCMAudio(info['url'])
-async def play(ctx):
-    channel = discord.utils.get(ctx.guild.channels, type=discord.VoiceChannel)
-    voice_client = await channel.connect()
-    await voice.create_ytdl_player("youtube link")
-    voice_client.play(player)
-"""
 @bot.command()
 async def yeet(ctx):
     """Sends a simple Hello Message"""
     await ctx.send("Sup!!")
 @bot.command()
 async def mime(ctx, *, something):
-	"""Repeats What you Say!"""
     await ctx.send(something)
 #bot.remove_command('help')
 #@bot.command()
@@ -33,15 +24,12 @@ async def mime(ctx, *, something):
 	#await ctx.send("Figure it out Yourself!")
 @bot.command()
 async def cult(ctx, member: discord.User):
-	"""Didnt fix that one"""
     await member.send("Wanna Join the Illuminati?")
 @bot.command()
 async def shout(ctx):
-	"""Mentions"""
 	await ctx.send(ctx.author.mention)
 @bot.command()
 async def mention(ctx):
-	"""Mention"""
     await ctx.author.send(ctx.author.mention)
 @bot.event
 async def on_member_join(member):
@@ -49,7 +37,6 @@ async def on_member_join(member):
     await member.send("Welcome to {}!".format(guild.name))
 @bot.command()
 async def add(ctx,a,b):
-	"""Simple Addition"""
 	c=int(a)+int(b)
 	await ctx.send(c)
 
@@ -59,5 +46,6 @@ async def on_command_error(ctx, exception):
 		await ctx.send("Cant do that mate")
 	
 bot.run("NDM0MDE5OTc4Nzg2ODk3OTMw.DbJaAg.ZMYTYDeGStoTJwsBpLBa8LD8cow") 
+
 
 
