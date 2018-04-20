f0c290aa006e54a699f251735142d5ec08187094
Abhinav <30766543+SaintOfAges@users.noreply.github.com>
2018-04-20 19:46:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,10 +56,11 @@ async def owner(ctx):
 @commands.has_any_role('Sigurd', 'Jacques', 'DR', 'beebee', 'chokkers delight', 'Alexa', 'Lava', 'Norway', 'jacob', 'couch', 'Brutally', 'Hunt', 'Cops', 'bob', 'DJ V/SA', 'Alena', 'new role', 'Shrew', 'Tequila', 'Xam', 'Karlie', 'Monday Meme', 'musik', 'Usagirl', 'Batman', 'Random', 'Octavia', 'Pikachu', 'Jacq', 'Perolina', 'soul', 'Riddle Honor', 'Mallu', 'Bots', 'Dark Kun', 'Labeeb', 'Pain', 'Pop', 'DJ','Members')
 async def cool(ctx):
     await ctx.send('You are cool indeed')
+
 @bot.event
-async def on_member_leave(member):
-    guild = member.guild
-    await member.send("You have left {}!".format(guild.name))
+async def on_server_role_update(before, after)
+    await ctx.send('Roles are being Changed!')
+	
 
 bot.run("NDM0MDE5OTc4Nzg2ODk3OTMw.DbJaAg.ZMYTYDeGStoTJwsBpLBa8LD8cow") 
 
